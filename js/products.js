1909ec744581bf7374f2d49195d5252e02cf11ff
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-08-12 18:41:29 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,5 +2,11 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(LIST_URL).then(function(resultObj){
+        if (resultObj.status === "ok") {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
