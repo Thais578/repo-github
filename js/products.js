6bb76169bedce7f96455cc7427e3b072209ff612
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-08-20 20:00:37 -0300

    Cambios.

--- js/products.js
+++ js/products.js
@@ -1,10 +1,46 @@
+const ORDER_ASC_BY_PRICE = "PMPm";
+const ORDER_DESC_BY_PRICE= "PmPM";
+const ORDER_BY_PROD_SOLD = "Cant.";
 var categoriesArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
+
+function sortCategories(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRICE)
+    {
+        result = array.sort(function(a, b) {
+            if ( a.price < b.price ){ return -1; }
+            if ( a.price > b.price ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_DESC_BY_PRICE){
+        result = array.sort(function(a, b) {
+            if ( a.price > b.price ){ return -1; }
+            if ( a.price < b.price ){ return 1; }
+            return 0;
+        });
+    }else if (criteria === ORDER_BY_PROD_COUNT){
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.productSold);
+            let bCount = parseInt(b.productSold);
+
+            if ( aCount > bCount ){ return -1; }
+            if ( aCount < bCount ){ return 1; }
+            return 0;
+        });
+    }
+
+    return result;
 
 function showCategoriesList(array){
 
     let htmlContentToAppend = "";
     for(let i = 0; i < array.length; i++) {
         let product = array[i];
+        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productSold) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productSold) <= maxCount))){
 
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
@@ -40,3 +76,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
     });
 });
+
+}
+
+
+
+    
+
+    
