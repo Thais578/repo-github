2f1b7f77f6fefccdc76e2c7cc01cda1b18c3e42a
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-08-28 09:25:47 -0300

    change

--- js/init.js
+++ js/init.js
@@ -48,7 +48,7 @@ var keyUsuario= sessionStorage.getItem("keyUsuario")
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-  document.getElementById("keyUsuario").innerHTML= "keyUsuario"
+  document.getElementById("keyUsuario").innerHTML=  document.getElementById("keyUsuario")
   
 });
 
