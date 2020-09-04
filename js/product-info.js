f350dbbca71355e95f8d06be42497091d61357d6
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-09-04 09:33:27 -0300

    change

--- js/product-info.js
+++ js/product-info.js
@@ -1,19 +1,64 @@
-var categoriesArray = [];
+var product = {};
 
-function showCategoriesList(array){
+function images(array){
 
     let htmlContentToAppend = "";
-}
 
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("images").innerHTML = htmlContentToAppend;
+    }
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok") {
-            categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray);
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
+            let productCategoryHTML = document.getElementById("productCategory");
+            let productCurrencyHTML = document.getElementById("productCurrency");
+            let productCostHTML = document.getElementById("productCost");
+            let productRelatedProductsHTML = document.getElementById("productRelatedProducts");
+            
+        
+            productNameHTML.innerHTML = product.name;
+           productDescriptionHTML.innerHTML = product.description;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCategoryHTML.innerHTML = product.category;
+            productCurrencyHTML.innerHTML = product.currency;
+            productCostHTML.innerHTML = product.cost;
+            productRelatedProductsHTML.innerHTML = product.relatedProducts;
+
+            //Muestro las imagenes en forma de galería
+            images(product.images);
+        }
+    });
+});
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
         }
     });
 });
