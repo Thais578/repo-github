fb913bfb34dacc31ca5c34c7d9d0a04617777b9d
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-09-16 18:42:19 -0300

    change

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function(e){
     
   });
   
+  //function cerrarsesion() {
+    //document.getElementById("keyUsuario4").innerHTML=  miStorage.getItem("keyUsuario")
+    //sessionStorage.removeItem("keyUsuario")
+  //}
   
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
