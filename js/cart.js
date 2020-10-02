a4f52d8287b33c6966d052cca624c6bc2c0c7ffb
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-10-02 09:48:06 -0300

    changes

--- js/cart.js
+++ js/cart.js
@@ -2,32 +2,44 @@ document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("keyUsuario6").innerHTML=  miStorage.getItem("keyUsuario")
     
   });
-  
-  function cartProducts() {
-    let articles = 
-    `
-    <a href="product-info.html" div class="list-group-item list-group-item-action">
+  var articlesArray= [];
+
+  function cartProducts(array) {
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.articles.length; i++){
+        let articles = array.articles[i];
+
+        htmlContentToAppend += `
+    
+    <div class="container">
     <div class="row">
             <div class="col-3">
-                <img src="` + articles.imgSrc + `" alt="` + "" + `" class="img-thumbnail">
+                <img src="` + articles.src + `" alt="` + "" + `" class="img-thumbnail">
             </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
                     <h4 class="mb-1">`+ articles.name +`</h4>
-                    <small class="text-muted">Moneda:`+ articles.currency + ``+ articles.Unitcost + `</br>Cantidad:` + articles.count + ` </small>
+                    <small class="text-muted">Moneda:`+ articles.currency + ``+ articles.unitCost + `</br>Cantidad:` + articles.count + ` </small>
                 </div>
             </div>
         </div>
     </div>
     `
-    document.getElementById('cart').innerHTML = articles;
+    document.getElementById('cart').innerHTML = htmlContentToAppend;
+    }
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-getJSONData(CART_INFO_URL).then(function(resultArt) {
-  if (resultArt.status === "ok") {
-let articles = result.data
-      cartProducts(articles);
+document.addEventListener("DOMContentLoaded", function(e) {
+getJSONData(CART_INFO_URL).then(function(resultObj) {
+  if (resultObj.status === "ok") {
+
+articlesArray = resultObj.data;
+      cartProducts(articlesArray);
   }
-})};
+});
+    });
