186aa12dd401ecf112747c9e372e4d05f2d4507e
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-10-02 01:16:24 -0300

    changes

--- js/cart.js
+++ js/cart.js
@@ -4,30 +4,30 @@ document.addEventListener("DOMContentLoaded", function(e){
   });
   
   function cartProducts() {
-    let product = 
+    let articles = 
     `
     <a href="product-info.html" div class="list-group-item list-group-item-action">
     <div class="row">
             <div class="col-3">
-                <img src="` + product.imgSrc + `" alt="` + "" + `" class="img-thumbnail">
+                <img src="` + articles.imgSrc + `" alt="` + "" + `" class="img-thumbnail">
             </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">`+ product.name +`</h4>
-                    <small class="text-muted">Moneda:`+ product.currency + ``+ product.Unitcost + `</br>Cantidad:` + product.count + ` </small>
+                    <h4 class="mb-1">`+ articles.name +`</h4>
+                    <small class="text-muted">Moneda:`+ articles.currency + ``+ articles.Unitcost + `</br>Cantidad:` + articles.count + ` </small>
                 </div>
             </div>
         </div>
     </div>
     `
-    document.getElementById('cart').innerHTML = product;
+    document.getElementById('cart').innerHTML = articles;
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-getJSONData(CART_INFO_URL).then(function(resultProd) {
-  if (resultProd.status === "ok") {
-let product = resultProduct.data
-      cartProducts(product);
+getJSONData(CART_INFO_URL).then(function(resultArt) {
+  if (resultArt.status === "ok") {
+let articles = result.data
+      cartProducts(articles);
   }
 })};
