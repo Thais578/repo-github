d783d6b4d24f4541b19ea79e506f334664dd6079
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-10-09 21:53:25 -0300

    changes

--- js/cart.js
+++ js/cart.js
@@ -20,20 +20,37 @@ document.addEventListener("DOMContentLoaded", function(e){
             </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">`+ articles.name +`</h4> 
-                    <input type="text" style="width:50px;height:30px" id="fname" name="fname">
-                    <label for="fname"></label>
+                    <h4 class="mb-1">`+ articles.name +`</h4>
+                     </div>
+                     <select name="select" onchange="subTotal(this.value)">
+                     <option value="value1">1</option> 
+                    <option value ="2">`+ articles.count+`</option>  
+                    <option value="value3">3</option>
                     </div>
-                    <div> Moneda:`+" "+ articles.currency +" "+ ``+ articles.unitCost + ` </div>
+                    </select>
+                    <div> Moneda:`+" "+ articles.currency +" "+ ` </div>
+                    <div id="costo"> `+ articles.unitCost + ` </div>
                     <div>
                 </div>
             </div>
         </div>
     </div>
     `
+    
+    }
     document.getElementById('cart').innerHTML = htmlContentToAppend;
     }
-}
+    
+    function subTotal() {
+        var cantidad= document.getElementById("select").value
+        var costo= document.getElementById("costo").value
+        var cuenta = cantidad * costo;
+        
+    }
+    
+ 
+
+    
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -47,3 +64,4 @@ articlesArray = resultObj.data;
   }
 });
     });
+    
