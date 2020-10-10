bbb8e6795e52da4ece27cde9f6e5a85a1635c88c
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-10-10 20:52:33 -0300

    changes

--- js/cart.js
+++ js/cart.js
@@ -14,27 +14,73 @@ document.addEventListener("DOMContentLoaded", function(e){
         htmlContentToAppend += `
     
     <div class="container">
+    <thead>
+    <div class="container mb-4">
+      <div class="row">
+          <div class="col-12">
+              <div class="table-responsive">
+                  <table class="table table-striped">
+                      <thead>
+                          <tr>
+                              <th scope="col class="text-center">Imagen</th>
+                              <th scope="col">Producto</th>
+                              <th scope="col" class="text-center">Cantidad</th>
+                              <th scope="col" class="text-center">Precio</th>
+                              <th scope="col" class="text-center">Moneda</th>
+                              <th scope="col" class="text-center">Eliminar</th>
+
+
+                          </tr>
+                      </thead>
+
     <div class="row">
             <div class="col-3">
-                <img src="` + articles.src + `" alt="` + "" + `" class="img-thumbnail w-50">
-            </div>
-            <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h4 class="mb-1">`+ articles.name +`</h4>
-                     </div>
-                     <select name="select" id="cant" onchange="subTotal(this.value)">
-                     <option value="value1">1</option> 
-                    <option value ="2">`+ articles.count+`</option>  
-                    <option value="value3">3</option>
-                    </div>
-                    </select>
-                    <div> Moneda:`+" "+ articles.currency +" "+ ` </div>
-                    <div id="costo"> `+ articles.unitCost + ` </div>
-                    <div>
-                </div>
-            </div>
-        </div>
-    </div>
+            <tbody>
+            <tr>
+                <td> <img src="` + articles.src + `" alt="` + "" + `" class="img-thumbnail w-50"></td>
+                <td> <div class="d-flex w-100 justify-content-between"> <h4 class="mb-1">`+ articles.name +`</h4></td>
+                <td><select name="select" id="cant" onchange="subTotal(this.value)">
+                <option value="1">1</option> 
+               <option value ="2">`+ articles.count+`</option>  
+               <option value="3">3</option></td>
+                
+                <td class="text-right">  <div id="costo"> `+ articles.unitCost + ` </div></td>
+                <td class="text-right">   <div> `+ articles.currency +" "+ `  </div></td>
+                <td class="text-right"><button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> </button> </td>
+               
+            </tr>
+
+          <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Sub-Total</td>
+          <td class="text-right"> <div id="resultado"></div></td>
+      </tr>
+      
+      <tr>	
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td><strong>Total</strong></td>
+          <td class="text-right"><strong></strong></td>
+      </tr>
+  </tbody>
+</table>
+</div>
+</div>
+<div class="col mb-2">
+<div class="row">
+<div class="col-sm-12 col-md-6 text-right">
+  <button class="btn btn-secondary btn-sm">Comprar</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+
     `
     
     }
@@ -45,9 +91,9 @@ document.addEventListener("DOMContentLoaded", function(e){
         let cantidad= document.getElementById("cant").value;
         let costo= document.getElementById("costo").innerHTML;
         var cuenta = cantidad * costo;
-        console.log(cuenta)
+     document.getElementById("resultado").innerHTML = cuenta;
         
-    }
+    };
    
  
 
