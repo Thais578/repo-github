616ca3fda92feeb71e812189f1a11d3257fb34c9
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-10-29 21:18:55 -0300

    changes

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,8 +3,13 @@ document.addEventListener("DOMContentLoaded", function(e){
     
   });
   
-  
-
+var miStorage= window.sessionStorage;
+function perfil() {
+var inputNombres= document.getElementById("inputNombres").value;
+var inputApellidos= document.getElementById("inputApellidos").value;
+  sessionStorage.setItem("keyNombres",inputNombres);
+  sessionStorage.setItem("keyApellidos",inputApellidos);
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
