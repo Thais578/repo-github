e448f9a2fbd077ba809d173f754c364c1b4ff7d1
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-11-02 17:09:40 -0300

    changes

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,13 +3,18 @@ document.addEventListener("DOMContentLoaded", function(e){
     
   });
   
-var miStorage= window.sessionStorage;
-function perfil() {
-var inputNombres= document.getElementById("inputNombres").value;
-var inputApellidos= document.getElementById("inputApellidos").value;
-  sessionStorage.setItem("keyNombres",inputNombres);
-  sessionStorage.setItem("keyApellidos",inputApellidos);
-}
+  var persona, myJSON, text, obj;
+
+  // Storing data:
+  persona = { name: "Emma", age: 31, city: "Montevideo" };
+  myJSON = JSON.stringify(persona);
+  sessionStorage.setItem("testJSON", myJSON);
+  
+  // Retrieving data:
+  text = sessionStorage.getItem("testJSON");
+  obj = JSON.parse(text);
+  document.getElementById("apellidos").innerHTML = obj.name;
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
