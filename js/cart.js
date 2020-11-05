4d72e3162805ac0d42adde5ae744239f946c4ebf
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-11-05 17:29:13 -0300

    changes

--- js/cart.js
+++ js/cart.js
@@ -523,7 +523,9 @@ function validarPais() {
                   }
 
                  
-      
+      function compraE() {
+        
+      }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
