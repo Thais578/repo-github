49d63ff41780f87bce0c73c3405a68edad00452a
Thais578 <68935800+Thais578@users.noreply.github.com>
2020-11-08 21:14:08 -0300

    changes

--- js/my-profile.js
+++ js/my-profile.js
@@ -19,52 +19,74 @@ persona.Telefono= document.getElementById("tel").value;
 
   }
  
-  text = localStorage.getItem("Persona");
-obj = JSON.parse(text);
-document.getElementById("demo").innerHTML = obj.Nombres;
- 
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo2").innerHTML = obj.Apellidos;
+  function validarNombres() {
+    var nombres= document.getElementById("nombres").value
   
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo3").innerHTML = obj.Email;
+    if(nombres=="") {
+      document.getElementById("errorNombres").innerHTML= "Ingrese un nombre";
+      return false;
+   
+   } else {
+    document.getElementById("errorNombres").style.display= "none";
+      return true
+   }}
 
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo4").innerHTML = obj.Edad;
+   function validarApellidos() {
+    var apellidos= document.getElementById("apellidos").value
   
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo5").innerHTML = obj.Telefono;
-  
-  
-function borrar() {
-  localStorage.clear()
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo").innerHTML = " <input name= name    class= form-control here placeholder= Nombres  type= text>"
+    if(apellidos=="") {
+      document.getElementById("errorApellidos").innerHTML= "Ingrese un Apellido";
+      return false;
+   
+   } else {
+    document.getElementById("errorApellidos").style.display= "none";
+      return true
+   }}
 
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo2").innerHTML = " <input name= name  class= form-control here placeholder= Apellidos  type= text>"
+   function validarEmail() {
+    var nombres= document.getElementById("email").value
   
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo3").innerHTML = " <input name= name  class= form-control here placeholder= Email  type= text>"
-  
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo4").innerHTML = " <input name= name  class= form-control here placeholder= Edad  type= text>"
-  
-  text = localStorage.getItem("Persona");
-  obj = JSON.parse(text);
-  document.getElementById("demo5").innerHTML = " <input name= name  class= form-control here placeholder= Teléfono  type= text>"
+    if(nombres=="") {
+      document.getElementById("errorEmail").innerHTML= "Ingrese un email";
+      return false;
+   
+   } else {
+    document.getElementById("errorEmail").style.display= "none";
+      return true
+   }}
 
-}
- 
+   function validarEdad() {
+    var nombres= document.getElementById("edad").value
   
+    if(nombres=="") {
+      document.getElementById("errorEdad").innerHTML= "Ingrese una edad";
+      return false;
+   
+   } else {
+    document.getElementById("errorEdad").style.display= "none";
+      return true
+   }}
+
+   function validarTel() {
+    var nombres= document.getElementById("tel").value
+  
+    if(nombres=="") {
+      document.getElementById("errorTel").innerHTML= "Ingrese un teléfono";
+      return false;
+   
+   } else {
+    document.getElementById("errorTel").style.display= "none";
+      return true
+   }}
+  
+    
+  function borrar() {
+    localStorage.removeItem("Persona")
+    text = localStorage.getItem("Persona");
+    obj = JSON.parse(text);
+    document.getElementById("nombres").innerHTML = obj.Nombres 
+    
+  }
 
    
 
@@ -73,5 +95,28 @@ function borrar() {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
- 
+  text = localStorage.getItem("Persona");
+  obj = JSON.parse(text);
+  document.getElementById("demo").innerHTML = obj.Nombres;
+   
+    text = localStorage.getItem("Persona");
+    obj = JSON.parse(text);
+    document.getElementById("demo2").innerHTML = obj.Apellidos;
+    
+    text = localStorage.getItem("Persona");
+    obj = JSON.parse(text);
+    document.getElementById("demo3").innerHTML = obj.Email;
+  
+    text = localStorage.getItem("Persona");
+    obj = JSON.parse(text);
+    document.getElementById("demo4").innerHTML = obj.Edad;
+    
+    text = localStorage.getItem("Persona");
+    obj = JSON.parse(text);
+    document.getElementById("demo5").innerHTML = obj.Telefono;
+    
 });
+
+
+ 
+  
